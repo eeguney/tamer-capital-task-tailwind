0e32c76b4ff9f220a3ebd53eb3e244f16257a24d
Emre Güney <emreguney@outlook.com.tr>
2022-04-03 18:28:16 +0300

    fix quote error on build

--- components/Step/Finish.js
+++ components/Step/Finish.js
@@ -3,7 +3,7 @@ const Finish = () => {
   return (
     <section className="p-8 lg:w-2/4 justify-center flex flex-col">
       <h3 className="text-xl lg:text-2xl mb-2 lg:mb-6 font-bold text-center">Tamamlandı</h3>
-      <p className="my-2 text-center">Form datalarına console'dan ulaşabilirsin.</p>
+      <p className="my-2 text-center">Form datalarına conosoledan ulaşabilirsin.</p>
     </section>
   );
 };
